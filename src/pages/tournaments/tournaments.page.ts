660a479222b7eadd96f1e84a2e0c93ca7e1dba39
Jesús Garrido Moscoso <jasus77@gmail.com>
2017-03-20 12:02:30 +0100

    ADD: Loading component to getTournaments method

--- src/pages/tournaments/tournaments.page.ts
+++ src/pages/tournaments/tournaments.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { LoadingController, NavController } from 'ionic-angular';
 
 import { TeamsPage } from '../pages';
 import { EliteAPI } from '../../services/services';
@@ -11,7 +11,9 @@ export class TournamentsPage {
 
   tournaments: any;
 
-  constructor(private  nav: NavController, private eliteAPI: EliteAPI) {
+  constructor(private  nav: NavController,
+              private eliteAPI: EliteAPI,
+              private loadingController: LoadingController) {
 
   }
 
@@ -20,7 +22,17 @@ export class TournamentsPage {
   }
 
   ionViewDidLoad(){
-    this.eliteAPI.getTournaments().then(data => this.tournaments = data);
+    let loader = this.loadingController.create({
+      content: 'Getting tournaments...'
+    });
+
+    loader.present().then(() => {
+      this.eliteAPI.getTournaments().then(data => {
+          this.tournaments = data;
+          loader.dismiss();
+      });
+    });
+
   }
 
 }
