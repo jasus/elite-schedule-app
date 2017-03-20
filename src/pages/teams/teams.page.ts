ca6248b65d1ea6519821eecc19ea01b7fd63c74f
Jesús Garrido Moscoso <jasus77@gmail.com>
2017-03-20 11:53:54 +0100

    ADD: API call with RxJS. Get Tournament data

--- src/pages/teams/teams.page.ts
+++ src/pages/teams/teams.page.ts
@@ -1,21 +1,28 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
 import { TeamHomePage } from '../pages';
+import { EliteAPI } from '../../services/services';
 
 @Component({
   templateUrl: 'teams.page.html'
 })
 export class TeamsPage {
 
-  teams = [
-    { id: 1, name: 'HC Elite' },
-    { id: 2, name: 'Team Takeover' },
-    { id: 3, name: 'DC Thunder' }
-  ];
+  teams = [];
 
-  constructor(private nav: NavController) {
+  constructor(private nav: NavController,
+              private navParams: NavParams,
+              private eliteAPI: EliteAPI) {
 
+  }
+
+  ionViewDidLoad(){
+    let selectedTournament = this.navParams.data;
+
+    this.eliteAPI.getTournamentData(selectedTournament.id).subscribe(data => {
+      this.teams = data.teams;
+    });
   }
 
   itemTapped($event, team){
--- src/services/elite-api.service.ts
+++ src/services/elite-api.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Http , Response } from '@angular/http';
 
+import 'rxjs';
+import { Observable } from 'rxjs/Observable';
+
 @Injectable()
 export class EliteAPI {
 
   private baseURL = 'https://elite-schedule-app-i2-397f4.firebaseio.com';
+
+  currentTournament: any = {};
+
+
   constructor(private http: Http) {}
 
   getTournaments(){
@@ -12,5 +19,13 @@ export class EliteAPI {
       this.http.get(`${this.baseURL}/tournaments.json`)
         .subscribe(res => resolve(res.json()));
     });
+  }
+
+  getTournamentData(tournamentId) : Observable<any>{
+    return this.http.get(`${this.baseURL}/tournaments-data/${tournamentId}.json`)
+      .map((response: Response) => {
+        this.currentTournament = response.json();
+        return this.currentTournament;
+      });
   }
 }
